71ada4d93f698c34e8f6c7bafdc65d8d1f47c90a
GilCND <fgilcnd@gmail.com>
2021-08-25 16:51:20 -0300

    Skeleton

---
+++ Project17-SpaceJunk/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project17-SpaceJunk
+//
+//  Created by Felipe Gil on 2021-08-25.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        
+        }
+        
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
